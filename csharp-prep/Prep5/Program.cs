dff924f6eaa98d814335d927820e43a084049229
Michael <michael_pitcher@rocketmail.com>
2024-11-20 20:34:39 -0700

    Finished Prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayMessage();
+    
+        Console.WriteLine("Please enter your name: ");
+        string username = Console.ReadLine();
+        DisplayPersonalMessage(username);
+        Console.WriteLine("Please enter your favorite number: ");
+        int favoritenumber = int.Parse(Console.ReadLine());
+        Square(favoritenumber, username);
     }
+    static void Square(int input,string username)
+    {
+        DisplayPersonalMessage(username);
+        int Square = input * input;
+        Console.WriteLine($"Your favorite number squared is {Square} ");
+
+    }
+static void DisplayMessage()
+{
+    Console.WriteLine("Welcome to the program!");
+}
+
+static void DisplayPersonalMessage(string username)
+{
+    Console.WriteLine($"Hello {username}");
+}
 }

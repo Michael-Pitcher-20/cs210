c593d4998fca79d11b4bdd527d54484b48f22326
Michael <michael_pitcher@rocketmail.com>
2024-11-20 19:22:13 -0700

    Finished Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        Random random = new Random();
+        int randomMagic = random.Next(1, 100);
+        int answer = randomMagic - 1;
+
+        while (answer != randomMagic)
+        {
+
+            Console.Write("What is the magic number?:");
+            answer = int.Parse(Console.ReadLine());
+
+            if (answer < randomMagic)
+            {
+                Console.WriteLine("Greater");
+            }
+            else if (answer > randomMagic)
+            {
+                Console.WriteLine("Less");
+            }
+            else
+            {
+                Console.WriteLine("You guessed the magic number!");
+            }
+        }
     }
 }

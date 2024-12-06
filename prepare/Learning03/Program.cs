3cddc7e4902385465e36cc258a1b71fa6c6b5b3f
Michael <michael_pitcher@rocketmail.com>
2024-12-05 18:53:13 -0700

    Finished Week 3 Learning activity

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Assignment a1 = new Assignment("Michael Pitcher", "Calculus");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Harry Potter", "Integrals", "9.8", "1-20");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Bilbo Baggins", "The ring", "The Hobbit:There and back again");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
